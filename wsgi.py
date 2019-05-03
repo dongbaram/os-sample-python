e75722e9a13d22d80fc73a28b163ddfa23c7304e
dongbaram <dongbaram@naver.com>
2019-05-03 11:23:00 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,10 @@
+    
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
-def run():
-    return "Main!!"
-
-@app.route("/test/")
-def test():
-    return "test"
-
-@app.route("/test2/")
-def test2():
-    return "test2"
+@application.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
